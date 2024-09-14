ca6b74c57ccecef640240798f69704e27db6c691
mayro.belizario <mayro.belizario@trinusco.com.br>
2024-09-14 15:36:57 -0300

    projeto inicial

---
+++ src/controller/AdotanteController.ts
@@ -0,0 +1,96 @@
+import { Request, Response } from "express";
+import AdotanteEntity from "../entities/AdotanteEntity";
+import AdotanteRepository from "../repositories/AdotanteRepository";
+import EnderecoEntity from "../entities/Endereco";
+import {
+  TipoRequestBodyAdotante,
+  TipoRequestParamsAdotante,
+  TipoResponseBodyAdotante,
+} from "../tipos/tiposAdotante";
+
+export default class AdotanteController {
+  constructor(private repository: AdotanteRepository) {}
+  async criaAdotante(
+    req: Request<TipoRequestParamsAdotante, {}, TipoRequestBodyAdotante>,
+    res: Response<TipoResponseBodyAdotante>
+  ) {
+    const { nome, celular, endereco, foto, senha } = <AdotanteEntity>req.body;
+
+    const novoAdotante = new AdotanteEntity(
+      nome,
+      senha,
+      celular,
+      foto,
+      endereco
+    );
+
+    await this.repository.criaAdotante(novoAdotante);
+    return res
+      .status(201)
+      .json({ data: { id: novoAdotante.id, nome, celular } });
+  }
+  async atualizaAdotante(
+    req: Request<TipoRequestParamsAdotante, {}, TipoRequestBodyAdotante>,
+    res: Response<TipoResponseBodyAdotante>
+  ) {
+    const { id } = req.params;
+    const { success, message } = await this.repository.atualizaAdotante(
+      Number(id),
+      req.body as AdotanteEntity
+    );
+
+    if (!success) {
+      return res.status(404).json({ error: message });
+    }
+
+    return res.sendStatus(204);
+  }
+
+  async listaAdotantes(
+    req: Request<TipoRequestParamsAdotante, {}, TipoRequestBodyAdotante>,
+    res: Response<TipoResponseBodyAdotante>
+  ) {
+    const listaDeAdotantes = await this.repository.listaAdotantes();
+    const data = listaDeAdotantes.map((adotante) => {
+      return {
+        id: adotante.id,
+        nome: adotante.nome,
+        celular: adotante.celular,
+      };
+    });
+    return res.json({ data });
+  }
+
+  async deletaAdotante(
+    req: Request<TipoRequestParamsAdotante, {}, TipoRequestBodyAdotante>,
+    res: Response<TipoResponseBodyAdotante>
+  ) {
+    const { id } = req.params;
+
+    const { success, message } = await this.repository.deletaAdotante(
+      Number(id)
+    );
+
+    if (!success) {
+      return res.status(404).json({ error: message });
+    }
+    return res.sendStatus(204);
+  }
+
+  async atualizaEnderecoAdotante(
+    req: Request<TipoRequestParamsAdotante, {}, TipoRequestBodyAdotante>,
+    res: Response<TipoResponseBodyAdotante>
+  ) {
+    const { id } = req.params;
+
+    const { success, message } = await this.repository.atualizaEnderecoAdotante(
+      Number(id),
+      req.body.endereco as EnderecoEntity
+    );
+
+    if (!success) {
+      return res.status(404).json({ error: message });
+    }
+    return res.sendStatus(204);
+  }
+}
---
+++ src/controller/PetController.ts
@@ -0,0 +1,114 @@
+import { Request, Response } from "express";
+import EnumEspecie from "../enum/EnumEspecie";
+import EnumPorte from "../enum/EnumPorte";
+import PetRepository from "../repositories/PetRepository";
+import PetEntity from "../entities/PetEntity";
+import {
+  TipoRequestBodyPet,
+  TipoRequestParamsPet,
+  TipoResponseBodyPet,
+} from "../tipos/tiposPet";
+
+export default class PetController {
+  constructor(private repository: PetRepository) {}
+  async criaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
+    const { adotado, especie, dataDeNascimento, nome, porte } = <PetEntity>(
+      req.body
+    );
+
+    if (!Object.values(EnumEspecie).includes(especie)) {
+      return res.status(400).json({ error: "Especie inválida" });
+    }
+
+    if (porte && !(porte in EnumPorte)) {
+      return res.status(400).json({ error: "Porte inválido" });
+    }
+    const novoPet = new PetEntity(
+      nome,
+      especie,
+      dataDeNascimento,
+      adotado,
+      porte
+    );
+
+    await this.repository.criaPet(novoPet);
+    return res
+      .status(201)
+      .json({ data: { id: novoPet.id, nome, especie, porte } });
+  }
+
+  async listaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
+    const listaDePets = await this.repository.listaPet();
+    const data = listaDePets.map((pet) => {
+      return {
+        id: pet.id,
+        nome: pet.nome,
+        porte: pet.porte,
+        especie: pet.especie,
+      };
+    });
+    return res.status(200).json({ data });
+  }
+
+  async atualizaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
+    const { id } = req.params;
+    const { success, message } = await this.repository.atualizaPet(
+      Number(id),
+      req.body as PetEntity
+    );
+
+    if (!success) {
+      return res.status(404).json({ error: message });
+    }
+    return res.sendStatus(204);
+  }
+
+  async deletaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
+    const { id } = req.params;
+
+    const { success, message } = await this.repository.deletaPet(Number(id));
+
+    if (!success) {
+      return res.status(404).json({ error: message });
+    }
+    return res.sendStatus(204);
+  }
+
+  async adotaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
+    const { pet_id, adotante_id } = req.params;
+
+    const { success, message } = await this.repository.adotaPet(
+      Number(pet_id),
+      Number(adotante_id)
+    );
+
+    if (!success) {
+      return res.status(404).json({ error: message });
+    }
+    return res.sendStatus(204);
+  }
+
+  async buscaPetPorCampoGenerico(req: Request, res: Response) {
+    const { campo, valor } = req.query;
+    const listaDePets = await this.repository.buscaPetPorCampoGenerico(
+      campo as keyof PetEntity,
+      valor as string
+    );
+    return res.status(200).json(listaDePets);
+  }
+}
